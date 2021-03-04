3d834a08210a3443b01f9801a2c259e88c3bc424
Nancy Do <nancy.do@slalom.com>
2021-03-03 23:24:15 -0600

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import Amplify from 'aws-amplify';
+import awsconfig from 'aws-exports';
+
+Amplify.configure(awsconfig);
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
